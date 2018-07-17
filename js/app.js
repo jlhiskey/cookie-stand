00e26500a5ca9016df56e79b491a98d37288d582
jlhiskey <jasonhi@crazyredhead.com>
2018-07-16 18:43:57 -0700

    added all locations

--- js/app.js
+++ js/app.js
@@ -25,7 +25,7 @@ var firstandPike = {
     }
   },
 
-//----------Calculates Cookies Per Hour----------------------------------------------------------------------------
+  //----------Calculates Cookies Per Hour----------------------------------------------------------------------------
 
   cookiesperHour: function () {
     var avgcookiesCalc = 0;
@@ -35,7 +35,7 @@ var firstandPike = {
     }
   },
 
-//----------Calculates Cookies Per Day---------------------------------------------------------------------------------
+  //----------Calculates Cookies Per Day---------------------------------------------------------------------------------
 
   totalcookiesSold: function () {
     var cookiesSoldCalc = 0;
@@ -45,7 +45,7 @@ var firstandPike = {
     this.totalcookiesSold = cookiesSoldCalc;
   },
 
-//----------Renders Cookies per hour on HTML------------------------------------------------------------------------
+  //----------Renders Cookies per hour on HTML------------------------------------------------------------------------
 
   showSales: function () {
 
@@ -57,13 +57,6 @@ var firstandPike = {
     }
   }
 };
-
-//----------Renders Total Cookies per hour on HTML-----------------------------------------------------------------
-
-var ulElem = document.getElementById('first-pike');
-var listItemElem = document.createElement('li');
-listItemElem.textContent = 'Total: ' + firstandPike.totalcookiesSold;
-ulElem.appendChild(listItemElem);
 
 //----------Merges All Functions to Shows on HTML------------------------------------------------------------------
 
@@ -78,5 +71,305 @@ function callFirstPike() {
 
 callFirstPike();
 
+//----------Renders Total Cookies per hour on HTML-----------------------------------------------------------------
 
+var ulElem = document.getElementById('first-pike');
+var listItemElem = document.createElement('li');
+listItemElem.textContent = 'Total: ' + firstandPike.totalcookiesSold;
+ulElem.appendChild(listItemElem);
 
+//-----------------------------------------------------------------------------------------------------------------
+//----------SeaTac Airport-----------------------------------------------------------------------------------------
+
+var seaTac = {
+  minCust: 3,
+  maxCust: 24,
+  avgcookiesperCust: 1.2,
+  custperHour: [],
+  soldperHour: [],
+  totalcookiesSold: 0,
+
+  //----------Calculates Random Customers Per Hour------------------------------------------------------------------
+
+  randcustperHour: function () {
+    for (var i = 0; i < storeHours.length; i++) {
+      var custperhourInt = Math.random() * (this.maxCust - this.minCust) + this.minCust;
+      custperhourInt = Math.round(custperhourInt);
+      this.custperHour.push(custperhourInt);
+    }
+  },
+
+  //----------Calculates Cookies Per Hour----------------------------------------------------------------------------
+
+  cookiesperHour: function () {
+    var avgcookiesCalc = 0;
+    for (var i = 0; i < storeHours.length; i++) {
+      avgcookiesCalc = Math.floor(this.custperHour[i] * this.avgcookiesperCust);
+      this.soldperHour.push(avgcookiesCalc);
+    }
+  },
+
+  //----------Calculates Cookies Per Day---------------------------------------------------------------------------------
+
+  totalcookiesSold: function () {
+    var cookiesSoldCalc = 0;
+    for (var i = 0; i < storeHours.length; i++) {
+      cookiesSoldCalc += this.soldperHour[i];
+    }
+    this.totalcookiesSold = cookiesSoldCalc;
+  },
+
+  //----------Renders Cookies per hour on HTML------------------------------------------------------------------------
+
+  showSales: function () {
+
+    var ulElem = document.getElementById('sea-tac');
+    for (var i = 0; i < storeHours.length; i++) {
+      var listItemElem = document.createElement('li');
+      listItemElem.innerHTML = storeHours[i] + ':<br> ' + this.soldperHour[i] + ' cookies';
+      ulElem.appendChild(listItemElem);
+    }
+  }
+};
+
+//----------Merges All Functions to Shows on HTML------------------------------------------------------------------
+
+function callseaTac() {
+  seaTac.randcustperHour();
+  seaTac.cookiesperHour();
+  seaTac.totalcookiesSold();
+  seaTac.showSales();
+}
+
+//----------Calls Functions to Render on HTML---------------------------------------------------------------------
+
+callseaTac();
+
+//----------Renders Total Cookies per hour on HTML-----------------------------------------------------------------
+
+var ulElem = document.getElementById('sea-tac');
+var listItemElem = document.createElement('li');
+listItemElem.textContent = 'Total: ' + seaTac.totalcookiesSold;
+ulElem.appendChild(listItemElem);
+
+//-----------------------------------------------------------------------------------------------------------------
+//----------Seattle Center-----------------------------------------------------------------------------------------
+
+var seaCenter = {
+  minCust: 11,
+  maxCust: 38,
+  avgcookiesperCust: 3.7,
+  custperHour: [],
+  soldperHour: [],
+  totalcookiesSold: 0,
+
+  //----------Calculates Random Customers Per Hour------------------------------------------------------------------
+
+  randcustperHour: function () {
+    for (var i = 0; i < storeHours.length; i++) {
+      var custperhourInt = Math.random() * (this.maxCust - this.minCust) + this.minCust;
+      custperhourInt = Math.round(custperhourInt);
+      this.custperHour.push(custperhourInt);
+    }
+  },
+
+  //----------Calculates Cookies Per Hour----------------------------------------------------------------------------
+
+  cookiesperHour: function () {
+    var avgcookiesCalc = 0;
+    for (var i = 0; i < storeHours.length; i++) {
+      avgcookiesCalc = Math.floor(this.custperHour[i] * this.avgcookiesperCust);
+      this.soldperHour.push(avgcookiesCalc);
+    }
+  },
+
+  //----------Calculates Cookies Per Day---------------------------------------------------------------------------------
+
+  totalcookiesSold: function () {
+    var cookiesSoldCalc = 0;
+    for (var i = 0; i < storeHours.length; i++) {
+      cookiesSoldCalc += this.soldperHour[i];
+    }
+    this.totalcookiesSold = cookiesSoldCalc;
+  },
+
+  //----------Renders Cookies per hour on HTML------------------------------------------------------------------------
+
+  showSales: function () {
+
+    var ulElem = document.getElementById('sea-center');
+    for (var i = 0; i < storeHours.length; i++) {
+      var listItemElem = document.createElement('li');
+      listItemElem.innerHTML = storeHours[i] + ':<br> ' + this.soldperHour[i] + ' cookies';
+      ulElem.appendChild(listItemElem);
+    }
+  }
+};
+
+//----------Merges All Functions to Shows on HTML------------------------------------------------------------------
+
+function callseaCenter() {
+  seaCenter.randcustperHour();
+  seaCenter.cookiesperHour();
+  seaCenter.totalcookiesSold();
+  seaCenter.showSales();
+}
+
+//----------Calls Functions to Render on HTML---------------------------------------------------------------------
+
+callseaCenter();
+
+//----------Renders Total Cookies per hour on HTML-----------------------------------------------------------------
+
+var ulElem = document.getElementById('sea-center');
+var listItemElem = document.createElement('li');
+listItemElem.textContent = 'Total: ' + seaCenter.totalcookiesSold;
+ulElem.appendChild(listItemElem);
+
+//-----------------------------------------------------------------------------------------------------------------
+//----------Capitol Hill-----------------------------------------------------------------------------------------
+
+var capHill = {
+  minCust: 20,
+  maxCust: 38,
+  avgcookiesperCust: 2.3,
+  custperHour: [],
+  soldperHour: [],
+  totalcookiesSold: 0,
+
+  //----------Calculates Random Customers Per Hour------------------------------------------------------------------
+
+  randcustperHour: function () {
+    for (var i = 0; i < storeHours.length; i++) {
+      var custperhourInt = Math.random() * (this.maxCust - this.minCust) + this.minCust;
+      custperhourInt = Math.round(custperhourInt);
+      this.custperHour.push(custperhourInt);
+    }
+  },
+
+  //----------Calculates Cookies Per Hour----------------------------------------------------------------------------
+
+  cookiesperHour: function () {
+    var avgcookiesCalc = 0;
+    for (var i = 0; i < storeHours.length; i++) {
+      avgcookiesCalc = Math.floor(this.custperHour[i] * this.avgcookiesperCust);
+      this.soldperHour.push(avgcookiesCalc);
+    }
+  },
+
+  //----------Calculates Cookies Per Day---------------------------------------------------------------------------------
+
+  totalcookiesSold: function () {
+    var cookiesSoldCalc = 0;
+    for (var i = 0; i < storeHours.length; i++) {
+      cookiesSoldCalc += this.soldperHour[i];
+    }
+    this.totalcookiesSold = cookiesSoldCalc;
+  },
+
+  //----------Renders Cookies per hour on HTML------------------------------------------------------------------------
+
+  showSales: function () {
+
+    var ulElem = document.getElementById('capitol-hill');
+    for (var i = 0; i < storeHours.length; i++) {
+      var listItemElem = document.createElement('li');
+      listItemElem.innerHTML = storeHours[i] + ':<br> ' + this.soldperHour[i] + ' cookies';
+      ulElem.appendChild(listItemElem);
+    }
+  }
+};
+
+//----------Merges All Functions to Shows on HTML------------------------------------------------------------------
+
+function callcapHill() {
+  capHill.randcustperHour();
+  capHill.cookiesperHour();
+  capHill.totalcookiesSold();
+  capHill.showSales();
+}
+
+//----------Calls Functions to Render on HTML---------------------------------------------------------------------
+
+callcapHill();
+
+//----------Renders Total Cookies per hour on HTML-----------------------------------------------------------------
+
+var ulElem = document.getElementById('capitol-hill');
+var listItemElem = document.createElement('li');
+listItemElem.textContent = 'Total: ' + capHill.totalcookiesSold;
+ulElem.appendChild(listItemElem);
+
+//-----------------------------------------------------------------------------------------------------------------
+//----------Alki Beach-----------------------------------------------------------------------------------------
+
+var alkiBeach = {
+  minCust: 2,
+  maxCust: 16,
+  avgcookiesperCust: 4.6,
+  custperHour: [],
+  soldperHour: [],
+  totalcookiesSold: 0,
+
+  //----------Calculates Random Customers Per Hour------------------------------------------------------------------
+
+  randcustperHour: function () {
+    for (var i = 0; i < storeHours.length; i++) {
+      var custperhourInt = Math.random() * (this.maxCust - this.minCust) + this.minCust;
+      custperhourInt = Math.round(custperhourInt);
+      this.custperHour.push(custperhourInt);
+    }
+  },
+
+  //----------Calculates Cookies Per Hour----------------------------------------------------------------------------
+
+  cookiesperHour: function () {
+    var avgcookiesCalc = 0;
+    for (var i = 0; i < storeHours.length; i++) {
+      avgcookiesCalc = Math.floor(this.custperHour[i] * this.avgcookiesperCust);
+      this.soldperHour.push(avgcookiesCalc);
+    }
+  },
+
+  //----------Calculates Cookies Per Day---------------------------------------------------------------------------------
+
+  totalcookiesSold: function () {
+    var cookiesSoldCalc = 0;
+    for (var i = 0; i < storeHours.length; i++) {
+      cookiesSoldCalc += this.soldperHour[i];
+    }
+    this.totalcookiesSold = cookiesSoldCalc;
+  },
+
+  //----------Renders Cookies per hour on HTML------------------------------------------------------------------------
+
+  showSales: function () {
+
+    var ulElem = document.getElementById('alki-beach');
+    for (var i = 0; i < storeHours.length; i++) {
+      var listItemElem = document.createElement('li');
+      listItemElem.innerHTML = storeHours[i] + ':<br> ' + this.soldperHour[i] + ' cookies';
+      ulElem.appendChild(listItemElem);
+    }
+  }
+};
+
+//----------Merges All Functions to Shows on HTML------------------------------------------------------------------
+
+function callalkiBeach() {
+  alkiBeach.randcustperHour();
+  alkiBeach.cookiesperHour();
+  alkiBeach.totalcookiesSold();
+  alkiBeach.showSales();
+}
+
+//----------Calls Functions to Render on HTML---------------------------------------------------------------------
+
+callalkiBeach();
+
+//----------Renders Total Cookies per hour on HTML-----------------------------------------------------------------
+
+var ulElem = document.getElementById('alki-beach');
+var listItemElem = document.createElement('li');
+listItemElem.textContent = 'Total: ' + alkiBeach.totalcookiesSold;
+ulElem.appendChild(listItemElem);
