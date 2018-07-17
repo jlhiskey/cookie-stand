890e423ad5f78a083957d8ff3c9820559f24df9a
jlhiskey <jasonhi@crazyredhead.com>
2018-07-16 17:46:05 -0700

    finally built successful cookie function

--- js/app.js
+++ js/app.js
@@ -1,26 +1,62 @@
 'use strict';
+var storeHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 
-// var turtle = {
-//   legs: 4,
-//   shell: true,
-//   age: 115,
-//   colors: ['turquoise'],
-//   weight: 25,
-//   ninja: true,
-//   snap: function() {
-//     console.log('cowabunga!');
-//   },
-// };
+// // 1st and Pike.
+var firstPike = {
+  minimumCust: 23,
+  maximumCust: 65,
+  avgCookiesCust: 6.3,
+  custPerHour: [],
+  soldPerHour: [],
+  totalSold: 0,
 
+  randomCustPerHour: function () {
+    for (var i = 0; i < storeHours.length; i++) {
+      var custPerHourInt = Math.random() * (this.maximumCust - this.minimumCust) + this.minimumCust;
+      custPerHourInt = Math.round(custPerHourInt);
+      this.custPerHour.push(custPerHourInt);
+    }
+  },
 
-var months = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec'];
-var ulEl = document.createElement('ul');
+  cookiesPerHour: function () {
+    var avgCookiesCalc = 0;
+    for (var i = 0; i < storeHours.length; i++) {
+      avgCookiesCalc = Math.floor(this.custPerHour[i] * this.avgCookiesCust);
+      this.soldPerHour.push(avgCookiesCalc);
+    }
+  },
 
-for(var i = 0; i < months.length; i++) {
-  var liEl = document.createElement('li');
-  liEl.textContent = months[i];
-  ulEl.appendChild(liEl);
+  totalCookiesSold: function () {
+    var cookiesSoldCalc = 0;
+    for (var i = 0; i < storeHours.length; i++) {
+      cookiesSoldCalc += this.soldPerHour[i];
+    }
+    this.totalSold = cookiesSoldCalc;
+  },
+
+  renderSales: function () {
+    // Access the parent element from the DOM.
+    var ulElem = document.getElementById('first-pike');
+    for (var i = 0; i < storeHours.length; i++) {
+      // 1. Create element.
+      var listItemElem = document.createElement('li');
+      // 2. Give it content.
+      listItemElem.innerHTML = storeHours[i] + ':<br> ' + this.soldPerHour[i] + ' cookies';
+      //3. Append it to the DOM.
+      ulElem.appendChild(listItemElem);
+    }
+  }
+};
+function callFirstPike() {
+  firstPike.randomCustPerHour();
+  firstPike.cookiesPerHour();
+  firstPike.totalCookiesSold();
+  firstPike.renderSales();
 }
+callFirstPike();
 
-var monthsSection = document.getElementById('months');
-monthsSection.appendChild(ulEl);
+var ulElem = document.getElementById('first-pike');
+var listItemElem = document.createElement('li');
+listItemElem.textContent = 'Total: ' + firstPike.totalSold;
+ulElem.appendChild(listItemElem);
+
